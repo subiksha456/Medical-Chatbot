7c4e95d87066579097cd73f90c1ccad964fb6f86
subiksha456 <subikshass456@gmail.com>
2026-07-23 23:27:49 +0530

    Completed environment setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Subiksha",
+    author_email="subikshass456@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        
+    ]
+)
